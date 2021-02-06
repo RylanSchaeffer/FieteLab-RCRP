846d1d7f80731cd15035ef8f8e48772175524b1c
Rylan <rylanschaeffer@gmail.com>
2021-02-05 20:57:16 -0800

    Added CRP analytics

--- temp.py
+++ temp.py
@@ -1,9 +1,11 @@
+import joblib
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 import scipy.special
 import scipy.stats
+import seaborn as sns
 from sympy.functions.combinatorial.numbers import stirling
 
 
@@ -23,23 +25,23 @@ def draw_sample_from_crp(T, alpha):
 vectorized_draw_sample_from_crp = np.vectorize(draw_sample_from_crp,
                                                otypes=[np.ndarray])
 
-T = 200  # max time
-num_samples = 2000  # number of samples to draw from CRP(alpha)
+T = 50  # max time
+num_samples = 5000  # number of samples to draw from CRP(alpha)
 alphas = [1.1, 10.01, 30.03]  # CRP parameter
 crp_samples_by_alpha = {}
 for alpha in alphas:
-    crp_samples_path = f'crp_sample_{alpha}.npy'
-    if os.path.isfile(crp_samples_path):
-        crp_samples = np.load(crp_samples_path)
-        print(f'Loaded samples for {crp_samples_path}')
+    crp_empirics_path = f'crp_sample_{alpha}.npy'
+    if os.path.isfile(crp_empirics_path):
+        crp_samples = np.load(crp_empirics_path)
+        print(f'Loaded samples for {crp_empirics_path}')
         assert crp_samples.shape == (num_samples, T)
     else:
         crp_samples = vectorized_draw_sample_from_crp(
             T=np.full(shape=num_samples, fill_value=T),
             alpha=np.full(shape=num_samples, fill_value=alpha))
-        print(f'Generated samples for {crp_samples}')
+        print(f'Generated samples for {crp_empirics_path}')
         crp_samples = np.stack(crp_samples)
-        np.save(arr=crp_samples, file=crp_samples_path)
+        np.save(arr=crp_samples, file=crp_empirics_path)
     crp_samples_by_alpha[alpha] = crp_samples
 
 
@@ -47,46 +49,46 @@ def prob_kth_table_exists_at_time_t(t, k, alpha):
     if k > t:
         prob = 0.
     else:
-        prob = scipy.special.gamma(alpha)
+        prob = 1.
+        prob *= scipy.special.gamma(alpha)
+        prob *= stirling(n=t, k=k, kind=1, signed=False)
         prob /= scipy.special.gamma(alpha + t)
         prob *= np.power(alpha, k)
-        prob *= stirling(n=t, k=k, kind=1, signed=False)
     return prob
 
 
-def prob_tth_customer_at_table_k(t, k, alpha):
-    if t == 1:
-        if k == 1:
-            prob = 1.
-        else:
-            prob = 0.
-    if t == 2:
-        if k == 1:
-            prob = 1. / (1. + alpha)
-        elif k == 2:
-            prob = alpha / (1. + alpha)
-        else:
-            prob = 0.
+def construct_analytical_customer_probs_and_table_occupancies(T, alpha):
+    expected_num_tables = min(2 * int(alpha * np.log(1 + T / alpha)), T)
+    customer_seating_probs = np.zeros(shape=(T + 1, expected_num_tables + 1))
+    customer_seating_probs[1, 1] = 1.
+    customer_seating_probs[2, 1] = 1. / (1. + alpha)
+    customer_seating_probs[2, 2] = alpha / (1. + alpha)
+    for customer_num in range(3, T + 1):
+        customer_seating_probs[customer_num, :] += customer_seating_probs[customer_num - 1, :]
+        for table_num in range(1, expected_num_tables + 1):
+            diff = prob_kth_table_exists_at_time_t(t=customer_num - 1, k=table_num - 1, alpha=alpha) - \
+                   prob_kth_table_exists_at_time_t(t=customer_num - 2, k=table_num - 1, alpha=alpha)
+            scaled_diff = alpha * diff / (alpha + customer_num - 1)
+            customer_seating_probs[customer_num, table_num] += scaled_diff
+    analytical_table_occupancies = np.sum(customer_seating_probs, axis=0)
+    return customer_seating_probs[1:, 1:], analytical_table_occupancies[1:]
+
+
+# TODO: how is R code so much faster
+analytical_table_occupancies_by_alpha = {}
+for alpha in alphas:
+    crp_analytics_path = f'crp_analytics_{alpha}.npy'
+    if os.path.isfile(crp_analytics_path):
+        analytical_table_occupancies = np.load(crp_analytics_path)
     else:
-        prob = prob_tth_customer_at_table_k(t=2, k=k, alpha=alpha)
-        for tprime in range(2, t):
-            diff = prob_kth_table_exists_at_time_t(t=tprime, k=k-1, alpha=alpha) -\
-                   prob_kth_table_exists_at_time_t(t=tprime-1, k=k - 1, alpha=alpha)
-            prob = prob + alpha * diff / (alpha + t)
-    return prob
+        customer_seating_probs, analytical_table_occupancies = construct_analytical_customer_probs_and_table_occupancies(
+            T=T,
+            alpha=alpha)
+        np.save(arr=analytical_table_occupancies, file=crp_analytics_path)
+    analytical_table_occupancies_by_alpha[alpha] = analytical_table_occupancies
 
-
-# theoretical_table_occupancies = {}
-# for alpha in alphas:
-#     theoretical_table_occupancies[alpha] = {}
-#     for k in range(1, T):
-#         running_prob_sum = 0.
-#         for t in range(T):
-#             running_prob_sum += prob_tth_customer_at_table_k(t=t, k=k, alpha=alpha)
-#         theoretical_table_occupancies[alpha][k] = running_prob_sum
-# 
-# 
-# print(10)
+# vectorized_prob_tth_customer_at_table_k = np.vectorize(prob_tth_customer_at_table_k,
+#                                                        otypes=[np.ndarray])
 
 
 # def compute_unsigned_stirling_nums_of_first_kind(T):
@@ -97,29 +99,66 @@ def prob_tth_customer_at_table_k(t, k, alpha):
 #         for j in range(1, T):
 #             unsigned_stirling_matrix[i, j] = unsigned_stirling_matrix[i - 1, j - 1] - i * unsigned_stirling_matrix[i - 1, j]
 #     return np.abs(unsigned_stirling_matrix)
-
-
+#
+#
 # unsigned_stirling_matrix = compute_unsigned_stirling_nums_of_first_kind(T=T)
 
 
-num_traces = 200
 table_nums = 1 + np.arange(T)
 for alpha, crp_samples in crp_samples_by_alpha.items():
-    plot_cutoff = alpha * np.log(1 + T / alpha)
+    table_cutoff = alpha * np.log(1 + T / alpha)
     empiric_table_occupancies_mean = np.mean(crp_samples, axis=0)
     empiric_table_occupancies_sem = scipy.stats.sem(crp_samples, axis=0)
-    plt.errorbar(x=table_nums,
-                 y=empiric_table_occupancies_mean,
-                 yerr=empiric_table_occupancies_sem,
-                 label=f'Empiric (N={num_samples})')
-    for i in range(num_traces):
-        plt.plot(table_nums, crp_samples[i, :], alpha=0.05, color='k')
+    fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(40, 10), sharey=True)
+    fig.suptitle(f'alpha={alpha}')
+    for i in range(200):
+        axes[0].plot(table_nums, crp_samples[i, :], alpha=0.01, color='k')
+    axes[0].scatter(x=table_nums,
+                    y=empiric_table_occupancies_mean,
+                    # yerr=empiric_table_occupancies_sem,
+                    # linewidth=2,
+                    # fmt='none',
+                    label=f'Empiric (N={num_samples}, alpha={alpha})')
+    axes[0].scatter(table_nums[:len(analytical_table_occupancies_by_alpha[alpha])],
+                    analytical_table_occupancies_by_alpha[alpha],
+                    # '--',
+                    marker='x',
+                    # linewidth=2,
+                    label=f'Analytic (alpha={alpha}, sum={np.sum(analytical_table_occupancies_by_alpha[alpha])})')
     print(f'Plotted alpha={alpha}')
-    plt.legend()
-    plt.title(f'alpha={alpha}')
-    plt.xlabel('Table Number')
-    plt.xlim(1, plot_cutoff)
-    # plt.xscale('log')
-    plt.ylabel('Mean Table Occupancy')
+    axes[0].legend()
+    axes[0].set_xlabel('Table Number')
+    axes[0].set_ylabel('Mean Table Occupancy')
+    axes[0].set_xlim(1, table_cutoff)
+
+    for i in range(20):
+        axes[1].plot(table_nums, crp_samples[i, :])
+    axes[1].set_xlabel('Table Number')
+    axes[1].set_ylabel('Mean Table Occupancy')
+    axes[1].set_xlim(1, table_cutoff)
+
+    table_cutoff = 10
+    table_idx = np.cumsum(np.ones_like(crp_samples[:, :table_cutoff]), axis=1)
+    crp_samples_df = pd.DataFrame({
+        'num_occupants': np.reshape(crp_samples[:, :table_cutoff], -1),
+        'table_idx': np.reshape(table_idx, -1)})
+    sns.boxenplot(x='table_idx',
+                  y='num_occupants',
+                  data=crp_samples_df,
+                  ax=axes[2])
+    axes[2].set_xlabel('Table Number')
+    axes[2].set_ylabel('Number of Occupants')
+    axes[2].set_xlim(-1, table_cutoff)
+
+    sns.violinplot(x='table_idx',
+                   y='num_occupants',
+                   data=crp_samples_df,
+                   ax=axes[3])
+    axes[3].set_xlabel('Table Number')
+    axes[3].set_ylabel('Number of Occupants')
+    axes[3].set_xlim(-1, table_cutoff)
+
+    plt.savefig(f'crp_{alpha}_summary.png',
+                # bbox_inches='tight',
+                dpi=300)
     plt.show()
-    plt.savefig(f'crp_sample_{alpha}.png')
