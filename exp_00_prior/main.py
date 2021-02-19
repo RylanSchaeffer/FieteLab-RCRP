62442e4ba9b965b2dd66608bb15dc06474a8a1a8
Rylan <rylanschaeffer@gmail.com>
2021-02-19 08:44:06 -0800

    Created plot of CRP prior recursion

--- exp_00_prior/main.py
+++ exp_00_prior/main.py
@@ -10,9 +10,7 @@ from sympy.functions.combinatorial.numbers import stirling
 
 from utils.data import vectorized_sample_sequence_from_crp
 
-
 np.random.seed(1)
-
 
 exp_dir = 'exp_00_prior'
 plot_dir = os.path.join(exp_dir, 'plots')
@@ -79,16 +77,16 @@ for t in table_nums:
             k=repeat_idx,
             alpha=alpha)
     table_distributions_by_T[t] = result
-    if t == 1 or t == T:
-        plt.plot(table_nums,
-                 table_distributions_by_T[t],
-                 label=f'T={t}',
-                 color=cmap(float(t) / T))
-    else:
-        plt.plot(table_nums,
-                 table_distributions_by_T[t],
-                 # label=f'T={t}',
-                 color=cmap(float(t) / T))
+    # if t == 1 or t == T:
+    #     plt.plot(table_nums,
+    #              table_distributions_by_T[t],
+    #              label=f'T={t}',
+    #              color=cmap(float(t) / T))
+    # else:
+    plt.plot(table_nums,
+             table_distributions_by_T[t],
+             # label=f'T={t}',
+             color=cmap(float(t) / T))
 # https://stackoverflow.com/questions/43805821/matplotlib-add-colorbar-to-non-mappable-object
 norm = mpl.colors.Normalize(vmin=1, vmax=T)
 sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
@@ -109,7 +107,8 @@ plt.show()
 
 def construct_analytical_customer_probs_and_table_occupancies(T, alpha):
     expected_num_tables = min(5 * int(alpha * np.log(1 + T / alpha)), T)
-    customer_seating_probs = np.zeros(shape=(T + 1, expected_num_tables + 1))
+    expected_num_tables = T
+    customer_seating_probs = np.zeros(shape=(T + 1, T + 1))
     customer_seating_probs[1, 1] = 1.
     customer_seating_probs[2, 1] = 1. / (1. + alpha)
     customer_seating_probs[2, 2] = alpha / (1. + alpha)
@@ -125,16 +124,22 @@ def construct_analytical_customer_probs_and_table_occupancies(T, alpha):
 
 
 analytical_table_occupancies_by_alpha = {}
+customer_seating_probs_by_alpha = {}
 for alpha in alphas:
-    crp_analytics_path = os.path.join(exp_dir, f'crp_analytics_{alpha}.npy')
+    crp_analytics_path = os.path.join(exp_dir, f'crp_analytics_{alpha}.npz')
     if os.path.isfile(crp_analytics_path):
-        analytical_table_occupancies = np.load(crp_analytics_path)
+        npz_file = np.load(crp_analytics_path)
+        customer_seating_probs = npz_file['customer_seating_probs']
+        analytical_table_occupancies = npz_file['analytical_table_occupancies']
     else:
         customer_seating_probs, analytical_table_occupancies = construct_analytical_customer_probs_and_table_occupancies(
             T=T,
             alpha=alpha)
-        np.save(arr=analytical_table_occupancies, file=crp_analytics_path)
+        np.savez(analytical_table_occupancies=analytical_table_occupancies,
+                 customer_seating_probs=customer_seating_probs,
+                 file=crp_analytics_path)
     analytical_table_occupancies_by_alpha[alpha] = analytical_table_occupancies
+    customer_seating_probs_by_alpha[alpha] = customer_seating_probs
 
 table_nums = 1 + np.arange(T)
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
@@ -169,6 +174,55 @@ plt.savefig(os.path.join(plot_dir, f'crp_table_occupancies.png'),
             bbox_inches='tight',
             dpi=300)
 plt.show()
+
+
+table_distributions_by_T_array = np.stack([table_distributions_by_T[key]
+                                           for key in sorted(table_distributions_by_T.keys())])
+fig, axes = plt.subplots(nrows=1,
+                         ncols=5,
+                         figsize=(18, 4),
+                         gridspec_kw={"width_ratios": [1, 0.1, 1, 0.1, 1]},
+                         sharex=True)
+
+ax = axes[0]
+sns.heatmap(
+    data=np.cumsum(customer_seating_probs_by_alpha[30.03], axis=0).T,
+    ax=ax,
+    cbar_kws=dict(label=r'$\sum_{t^{\prime} = 1}^{t-1} p(z_{t\prime} = k)$'),
+    cmap='jet')
+ax.invert_yaxis()
+ax.set_xlabel('Time Index')
+ax.set_ylabel('Table Index')
+ax.set_title('Running Sum of Prev. Tables\' Distributions')
+
+axes[1].axis('off')
+
+ax = axes[2]
+sns.heatmap(
+    data=table_distributions_by_T_array.T,
+    ax=ax,
+    cbar_kws=dict(label='$p(K_t = k)$'),
+    cmap='jet')
+ax.invert_yaxis()
+ax.set_title('Distribution over Number of Non-Empty Tables')
+ax.set_xlabel('Time Index')
+
+axes[3].axis('off')
+
+ax = axes[4]
+sns.heatmap(
+    data=customer_seating_probs_by_alpha[30.03].T,
+    ax=ax,
+    cbar_kws=dict(label='$p(z_t)$'),
+    cmap='jet')
+ax.invert_yaxis()
+ax.set_title('Distribution for New Table')
+ax.set_xlabel('Time Index')
+plt.savefig(os.path.join(plot_dir, 'crp_recursion_alpha=30.03.png'),
+            bbox_inches='tight',
+            dpi=300)
+plt.show()
+
 
 table_nums = 1 + np.arange(T)
 for alpha, crp_samples in crp_samples_by_alpha.items():
@@ -250,7 +304,8 @@ else:
                     T=np.full(shape=num_samples, fill_value=T),
                     alpha=np.full(shape=num_samples, fill_value=alpha))
                 table_occupancies = np.stack(table_occupancies)
-                mse_empiric_vs_analytical_per_dataset[num_samples_idx, alpha_idx, repeat_idx, :len(analytical_table_occupancies)] \
+                mse_empiric_vs_analytical_per_dataset[num_samples_idx, alpha_idx, repeat_idx,
+                :len(analytical_table_occupancies)] \
                     = np.square(np.mean(table_occupancies, axis=0)[:len(analytical_table_occupancies)]
                                 - analytical_table_occupancies)
 
