2cb0ffb69c8837f1c039f127363e11d04b39726b
Rylan <rylanschaeffer@gmail.com>
2021-04-18 16:06:47 -0700

    Added 08 Omniglot to rsync pull

--- scripts/rsync_pull.sh
+++ scripts/rsync_pull.sh
@@ -1,8 +1,9 @@
 echo -n password:
 read -s password
 
-sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_00_crp_prior/plots/ exp_00_crp_prior/plots/
+#sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_00_crp_prior/plots/ exp_00_crp_prior/plots/
 sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_01_mixture_of_gaussians/plots/ exp_01_mixture_of_gaussians/plots
 sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_02_mixture_of_unigrams/plots/ exp_02_mixture_of_unigrams/plots/
-#sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_03_language_modeling/plots/ exp_03_language_modeling/plots/
+sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_03_language_modeling/plots/ exp_03_language_modeling/plots/
 #sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_07_olfactory/ exp_07_olfactory/
+sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_08_omniglot/plots/ exp_08_omniglot/plots/
