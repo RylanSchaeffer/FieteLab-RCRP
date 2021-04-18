7193d52016eed9c52e446b7fdce8cd3f33fef82f
Rylan <rylanschaeffer@gmail.com>
2021-04-18 12:14:42 -0700

    Updated rsync pull script to only pull plots

--- scripts/rsync_pull.sh
+++ scripts/rsync_pull.sh
@@ -1,9 +1,8 @@
 echo -n password:
 read -s password
 
-#sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_00_crp_prior/ exp_00_crp_prior/
-sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_01_mixture_of_gaussians/ exp_01_mixture_of_gaussians/
-sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_02_mixture_of_unigrams/ exp_02_mixture_of_unigrams/
-#sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_05_language_modeling/ exp_05_language_modeling/
-#sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_06_ibp_prior/ exp_06_ibp_prior/
+sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_00_crp_prior/plots/ exp_00_crp_prior/plots/
+sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_01_mixture_of_gaussians/plots/ exp_01_mixture_of_gaussians/plots
+sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_02_mixture_of_unigrams/plots/ exp_02_mixture_of_unigrams/plots/
+#sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_03_language_modeling/plots/ exp_03_language_modeling/plots/
 #sshpass -v -p $password rsync -avh rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_07_olfactory/ exp_07_olfactory/
