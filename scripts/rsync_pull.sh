895a4ba0eea54630552993d03888153f3fe4daad
Rylan <rylanschaeffer@gmail.com>
2021-06-17 21:52:20 -0700

    Finally fixed OpenMind path

--- scripts/rsync_pull.sh
+++ scripts/rsync_pull.sh
@@ -1,9 +1,9 @@
 echo -n password:
 read -s password
 
-#sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_00_crp_prior/plots/ exp_00_crp_prior/plots/
-sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_01_mixture_of_gaussians/plots* exp_01_mixture_of_gaussians/
-sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_02_mixture_of_unigrams/plots* exp_02_mixture_of_unigrams/
-#sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_03_reddit/plots/ exp_03_reddit/plots/
-sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_04_newsgroup/plots* exp_04_newsgroup/
-sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-ddCRP/exp_08_omniglot/plots* exp_08_omniglot/
+#sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-RCRP/exp_00_crp_prior/plots/ exp_00_crp_prior/plots/
+sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-RCRP/exp_01_mixture_of_gaussians/plots* exp_01_mixture_of_gaussians/
+sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-RCRP/exp_02_mixture_of_unigrams/plots* exp_02_mixture_of_unigrams/
+#sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-RCRP/exp_03_reddit/plots/ exp_03_reddit/plots/
+sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-RCRP/exp_04_newsgroup/plots* exp_04_newsgroup/
+sshpass -v -p $password rsync -avh --exclude='*.joblib' rylansch@openmind7.mit.edu:/om2/user/rylansch/FieteLab-RCRP/exp_08_omniglot/plots* exp_08_omniglot/
